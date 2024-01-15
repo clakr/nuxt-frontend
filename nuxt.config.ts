4bb648304ae7fc36987bb280c9cb53e507cece63
clakr <clarktolosa@gmail.com>
2024-01-15 18:16:02 +0800

    working register

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+	devtools: { enabled: true },
+	nitro: {
+		routeRules: {
+			"/backend/**": {
+				proxy: "http://127.0.0.1:8000/**",
+			},
+		},
+	},
+});
---
+++ pages/register.vue
@@ -0,0 +1,62 @@
+<template>
+    <h1>register</h1>
+    <form @submit.prevent="handleSubmit">
+        <div>
+            <label for="name">name</label>
+            <input v-model="form.name" type="text" name="name" id="name">
+        </div>
+        <div>
+            <label for="email">email</label>
+            <input v-model="form.email" type="text" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">password</label>
+            <input v-model="form.password" type="password" name="password" id="password">
+        </div>
+        <div>
+            <label for="password_confirmation">password_confirmation</label>
+            <input v-model="form.password_confirmation" type="password" name="password_confirmation"
+                id="password_confirmation">
+        </div>
+        <button type="submit">register</button>
+    </form>
+</template>
+
+<script setup lang="ts">
+
+const router = useRouter()
+
+const form = reactive({
+    name: "Clark Tolosa",
+    email: "clarktolosa@gmail.com",
+    password: "password",
+    password_confirmation: "password",
+})
+
+async function handleSubmit(event: Event) {
+
+    const { error: cookieError } = await useFetch("/backend/sanctum/csrf-cookie", {
+        credentials: "include"
+    })
+
+    if (cookieError.value) return console.error(cookieError.value)
+
+    const form = event.target as HTMLFormElement
+    const formData = new FormData(form)
+    const body = Object.fromEntries(formData)
+
+    const { error: registerError } = await useFetch("/backend/register", {
+        credentials: "include",
+        headers: {
+            accept: "application/json",
+            "x-xsrf-token": useCookie("XSRF-TOKEN") || "NO_COOKIE"
+        },
+        method: "POST",
+        body
+    })
+
+    if (registerError.value) return console.error(registerError.value)
+
+    router.push("/users")
+}
+</script>
