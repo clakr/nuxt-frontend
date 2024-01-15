b41d30398112df2dbb6ae5ce72c9db36d8d94bcc
clakr <clarktolosa@gmail.com>
2024-01-15 18:15:54 +0800

    routes boilerplate

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <nav>
+        <div>
+            <NuxtLink to="/users">users</NuxtLink>
+        </div>
+        <div>
+            <NuxtLink to="/">login</NuxtLink>
+            <NuxtLink to="/register">register</NuxtLink>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+    display: flex;
+    justify-content: space-between;
+
+    &>div {
+        display: flex;
+        column-gap: .5rem;
+    }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>login</h1>
+    <form>
+        <div>
+            <label for="email">email</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div>
+            <label for="password">password</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit">login</button>
+    </form>
+</template>
